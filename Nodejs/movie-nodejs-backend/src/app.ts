3cc60e151741e05aab7973b976e5c5184e1732df
Yue Dong <ydong011@fiu.edu>
2024-02-28 10:05:23 -0600

    update

---
+++ Nodejs/movie-nodejs-backend/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 4231;
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
+
+/* 
+  & init project, install express;
+  $ npm init
+  $ npm i express
+  & nodemon with ts;
+  $ npm install -D nodemon typescript ts-node
+  & edit tsconfig.json;
+    "outDir": "./dist"  
+  & add start and build in package.josn;
+  	"start": "npx nodemon --exec ts-node ./src/app.ts",
+		"build": "tsc"
+  & dotenv to use process.env;
+  $ npm install dotenv
+  $ npm install --save-dev @types/dotenv
+*/
